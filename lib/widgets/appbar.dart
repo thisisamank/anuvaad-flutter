ffbae010256fc46961e81d2ff8d6a62a5bab5f82
gauransh <gauranshk21@gmail.com>
2021-03-28 12:13:18 +0530

    final working round 2

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -5,9 +5,23 @@ import 'package:url_launcher/url_launcher.dart';
 class AppWidgets {
   static AppBar getAppBar() => AppBar(
         elevation: 0,
-        title: Image.asset(
-          'images/logo.png',
-          height: 30,
+        title: Row(
+          children: [
+            Image.asset(
+              'images/logo.png',
+              height: 30,
+            ),
+            Center(
+              child: Text(
+                "ANUVAAD",
+                style: TextStyle(
+                    color: Color(0xFF025EF6),
+                    fontSize: 48,
+                    fontFamily: 'Samkarn'),
+              ),
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         ),
         actions: [
           Center(
